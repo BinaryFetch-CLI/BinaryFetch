a3c4a029d8ff14ea8824c58b934b0b25a1abb12b
InterCentury <234583143+InterCentury@users.noreply.github.com>
2026-09-03 10:41:11 +0600

    Refactor: add option to get int-line spacing value option from json

--- src/BinaryFetch/cpp/windows/core/config_management.cpp
+++ src/BinaryFetch/cpp/windows/core/config_management.cpp
@@ -368,6 +368,45 @@ std::string ConfigManager::getResetColor() const {
     return resolveColor("reset", "reset");
 }
 
+
+// nested int, read int data from json
+int ConfigManager::getNestedInt(
+    const std::string& rawModule,
+    const std::string& path,
+    int defaultValue) const
+{
+    std::string module = resolveSectionKey(rawModule);
+
+    if (!m_loaded || !m_config.contains(module))
+        return defaultValue;
+
+    std::vector<std::string> keys;
+    std::stringstream ss(path);
+    std::string key;
+
+    while (std::getline(ss, key, '.'))
+        keys.push_back(key);
+
+    if (!keys.empty())
+        keys[0] = resolveSubsectionKey(module, keys[0]);
+
+    nlohmann::json current = m_config[module];
+
+    for (const auto& k : keys)
+    {
+        if (!current.contains(k))
+            return defaultValue;
+
+        current = current[k];
+    }
+
+    if (current.is_number_integer())
+        return current.get<int>();
+
+    return defaultValue;
+}
+
+
 // ===================== LABEL RESOLUTION =====================
 std::string ConfigManager::getLabel(const std::string& rawSection, const std::string& key, const std::string& defaultLabel) const {
     std::string section = resolveSectionKey(rawSection);
